b6a65b7f86f8284633387498cdab2bcbcdf5d7b5
PixelBrewer <pixelbrewerstudios@gmail.com>
2025-02-16 16:59:01 -0800

    Completing the bookbot

--- src/main.py
+++ src/main.py
@@ -3,17 +3,29 @@ def count_words(file_contents):
 	return len(words)
 
 def count_characters(file_contents):
-	file_contents_lower = file_contents.lower()
-	c_dict = {}
-	for c in file_contents_lower:
-		c_dict[c] += 1
+	chars = {}
+	for char in file_contents.lower():
+		if char in chars:
+			chars[char] += 1
+		else:
+			chars[char] = 1
+	return chars
+
+def print_report(words, characters):
+	print("\nCharacter counts:")
+	for char, count in sorted (characters.items()):
+		if char.isalpha():
+			print(f"The '{char}': character appears {count} times")
+		else:
+			# Ignore non-alphabetic characters
+			continue
 
 def main():
 	with open("books/frankenstein.txt") as f:
 		file_contents = f.read()
 		words = count_words(file_contents)
 		characters = count_characters(file_contents)
-		print (f"Words: {words}, Characters: {characters}")
+		print_report(words, characters)
 
 
 main()
